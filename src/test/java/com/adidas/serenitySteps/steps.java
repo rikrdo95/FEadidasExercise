a7783c35468c646b97d8aac4864161cc66202613
Ricardo Villanueva <rikrdo95@hotmail.es>
2021-11-27 18:56:44 +0100

    Added missing steps and refactorized some cucumber steps

--- src/test/java/com/adidas/serenitySteps/steps.java
+++ src/test/java/com/adidas/serenitySteps/steps.java
@@ -3,7 +3,33 @@ package com.adidas.serenitySteps;
 import com.adidas.serenitySteps.steps;
 import com.adidas.utils.utils;
 
+import cucumber.api.DataTable;
+
 public class steps {
+
+  public static void iNavigateToTheSectionsSECTIONS(String section) {
+  }
+
+  public static void iAddToTheCartTheProductPRODUCT(String product) {
+  }
+
+  public static void iDeleteFromTheCartTheProductPRODUCT(String product) {
+  }
+
+  public static void iWantToPlaceTheOrder() {
+  }
+
+  public static void iFillTheFormFields() {
+  }
+
+  public static void iClickOnELEMENT(String element) {
+  }
+
+  public static void iCheckTheFields(DataTable fields) {
+  }
+
+  public static void iClickOnOkToFinishThePurchase() {
+  }
 
   private static void wait(int milis) {
     try {
@@ -13,6 +39,4 @@ public class steps {
     }
   }
 
-public static void iNavigateToTheSectionsSECTIONS(String section) {
-}
 }
--- src/test/java/com/adidas/stepsDefinitions/stepsDefinitions.java
+++ src/test/java/com/adidas/stepsDefinitions/stepsDefinitions.java
@@ -2,6 +2,7 @@ package com.adidas.stepsDefinitions;
 
 import com.adidas.serenitySteps.steps;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.Given;
@@ -9,10 +10,45 @@ import cucumber.api.java.en.Given;
 public class stepsDefinitions {
 
   @Given("^I navigate to the section \"([^\"]*)\"$")
+  @And("^I navigate to the section \"([^\"]*)\"$")
+  @Then("^I navigate to the section \"([^\"]*)\"$")
   public void iNavigateToTheSectionSECTION(String section) {
     steps.iNavigateToTheSectionsSECTIONS(section);
   }
 
-  
+  @And("^I add to the cart the product \"([^\"]*)\"$")
+  public void iAddToTheCartTheProductPRODUCT(String product) {
+    steps.iAddToTheCartTheProductPRODUCT(product);
+  }
+
+  @Then("^I delete from the cart the product \"([^\"]*)\"$")
+  public void iDeleteFromTheCartTheProductPRODUCT(String product) {
+    steps.iDeleteFromTheCartTheProductPRODUCT(product);
+  }
+
+  @When("^I want to place the order$")
+  public void iWantToPlaceTheOrder() {
+    steps.iWantToPlaceTheOrder();
+  }
+
+  @And("^I fill the form fields$")
+  public void iFillTheFormFields() {
+    steps.iFillTheFormFields();
+  }
+
+  @And("^I click on \"([^\"]*)\"$")
+  public void iClickOnELEMENT(String element) {
+    steps.iClickOnELEMENT(element);
+  }
+
+  @Then("^I check the field(?s)$")
+  public void iCheckTheFields(DataTable fields) {
+    steps.iCheckTheFields(fields);
+  }
+
+  @And("^I click on Ok to finish the purchase$")
+  public void iClickOnOkToFinishThePurchase() {
+    steps.iClickOnOkToFinishThePurchase();
+  }
 
 }
