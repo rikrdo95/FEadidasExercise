0160b89bf7d8983daf9ca67bb765cf5133885067
rikrdo95 <94994565+rikrdo95@users.noreply.github.com>
2021-11-30 13:15:21 +0100

    Update HomePage.java
    Added protection against StaleElementReferenceException when trying to add a product to the cart

--- src/test/java/com/adidas/pageObjects/HomePage.java
+++ src/test/java/com/adidas/pageObjects/HomePage.java
@@ -1,7 +1,6 @@
 package com.adidas.pageObjects;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,12 +17,22 @@ public class HomePage extends BasePage {
     @Step
     public void goToSection(String section) {
         click(By.linkText(section));
+        // Wait for the first element to appear
+        waitVisibility(By.xpath("//*[@id=\"tbodyid\"]/div[1]/div/div/h4/a"));
     }
 
     @Step
     public void addProductToCart(String product) {
-        waitVisibility(By.linkText(product));
-        click(By.linkText(product));
+        try {
+            waitVisibility(By.linkText(product));
+            click(By.linkText(product));
+        } catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            // Sometimes the element is reloaded by the page, so when we try to click it
+            // the reference is gone and provoques a StaleElementReferenceException. In this
+            // case I try to get the element again
+            waitVisibility(By.linkText(product));
+            click(By.linkText(product));
+        }
 
         waitVisibility(By.linkText("Add to cart"));
         click(By.linkText("Add to cart"));
